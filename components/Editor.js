8e2c3ba21e19c52bbb31063557a966e82022130e
Damián Silvani <munshkr@gmail.com>
2018-12-17 19:43:06 -0300

    Save/load content to local storage

--- components/Editor.js
+++ components/Editor.js
@@ -3,17 +3,63 @@ import AceEditor from "react-ace";
 import "brace/mode/javascript";
 import "brace/theme/github";
 
-export default ({ onEval }) => (
-  <AceEditor
-    mode="javascript"
-    theme="github"
-    name="editor"
-    commands={[
-      {
-        name: "evaluate",
-        bindKey: { win: "Ctrl-Enter", mac: "Command-Enter" },
-        exec: onEval
-      }
-    ]}
-  />
-);
+const style = {
+  width: "100vw",
+  height: "100vh"
+};
+
+class Editor extends React.Component {
+  state = {
+    content: ""
+  };
+
+  componentDidMount() {
+    let content = this._getLastContent();
+    if (!content) {
+      content = this.props.defaultContent;
+    }
+    this.setState({ content: content });
+  }
+
+  _getLastContent() {
+    return localStorage.getItem("lastContent");
+  }
+
+  _setLastContent(content) {
+    localStorage.setItem("lastContent", content);
+  }
+
+  _onChange = text => {
+    this._setLastContent(text);
+  };
+
+  render() {
+    const { onEval, children } = this.props;
+    const { content } = this.state;
+
+    return (
+      <AceEditor
+        mode="javascript"
+        theme="github"
+        name="editor"
+        showGutter={false}
+        showPrintMargin={false}
+        fontSize={24}
+        focus={true}
+        value={content}
+        style={style}
+        onChange={this._onChange}
+        editorProps={{ $blockScrolling: true }}
+        commands={[
+          {
+            name: "evaluate",
+            bindKey: { win: "Ctrl-Enter", mac: "Command-Enter" },
+            exec: onEval
+          }
+        ]}
+      />
+    );
+  }
+}
+
+export default Editor;
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,18 @@ import dynamic from "next/dynamic";
 
 const Editor = dynamic(() => import("../components/Editor"), { ssr: false });
 
+const DEFAULT_CONTENT = `// Press Ctrl+Enter or Command+Enter to evaluate and hear a sine...
+s(t)`;
+
 class Index extends React.Component {
-  _onEval = e => {
-    console.log(e);
+  _onEval = editor => {
+    console.log("Evaluate!");
   };
 
   render() {
     return (
       <div>
-        <Editor onEval={this._onEval} />
+        <Editor onEval={this._onEval} defaultContent={DEFAULT_CONTENT} />
       </div>
     );
   }

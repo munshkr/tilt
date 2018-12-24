0a3ccad4ee81df4adf9ed2546c2c0f8c86b16aa0
Damián Silvani <munshkr@gmail.com>
2018-12-23 22:07:09 -0300

    Debugging load content (3)

--- pages/index.js
+++ pages/index.js
@@ -101,7 +101,7 @@ class Index extends React.Component {
   componentDidMount() {
     let content;
     const { query } = this.props.router;
-    console.log(`query params = ${query}`);
+    console.log(`query params = ${JSON.stringify(query)}`);
 
     // If URL contains a "c" param, decode source code
     if (query.c) {

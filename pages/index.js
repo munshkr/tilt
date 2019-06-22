a9487183fbd4b0ad4996fa630b47d6f078bd5a61
Damián Silvani <munshkr@gmail.com>
2019-06-22 11:51:07 -0300

    Refactor Index page component

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import Head from "next/head";
-import dynamic from "next/dynamic";
-import { withRouter } from "next/router";
-import getConfig from "next/config";
+import React from 'react';
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import { withRouter } from 'next/router';
+import getConfig from 'next/config';
 
-import lzwCompress from "lzwcompress";
+import lzwCompress from 'lzwcompress';
 
-import Button from "../components/Button";
-import SynthController from "../components/SynthController";
-import Oscilloscope from "../components/Oscilloscope";
-import ErrorMessage from "../components/ErrorMessage";
+import Button from '../components/Button';
+import SynthController from '../components/SynthController';
+import Oscilloscope from '../components/Oscilloscope';
+import ErrorMessage from '../components/ErrorMessage';
 
 const { publicRuntimeConfig } = getConfig();
 const { assetPrefix } = publicRuntimeConfig;
 
 const URL_VERSION = 1;
 
-const Editor = dynamic(() => import("../components/Editor"), {
+const Editor = dynamic(() => import('../components/Editor'), {
   ssr: false,
-  loading: () => <span>Loading...</span>
+  loading: () => <span>Loading...</span>,
 });
 
 const DEFAULT_CONTENT = `// Define variable o to set audio output, like this:
@@ -115,7 +115,7 @@ class Index extends React.Component {
     generator: null,
     isPlaying: false,
     isFlashing: false,
-    error: null
+    error: null,
   };
 
   editorRef = React.createRef();
@@ -140,7 +140,7 @@ class Index extends React.Component {
     // If URL contains a "c" param, decode source code
     if (query.c) {
       content = this._decodeCode(query.c);
-      console.log("load code from query params");
+      console.log('load code from query params');
     } else {
       // Otherwise, try to get last content from localStorage
       content = this._getLastContent();
@@ -148,7 +148,7 @@ class Index extends React.Component {
 
     // If any, set content
     if (content) {
-      console.log("found! set content");
+      console.log('found! set content');
       this.setState({ content: content });
     }
   }
@@ -156,7 +156,7 @@ class Index extends React.Component {
   _decodeCode(code) {
     try {
       const buf = atob(code)
-        .split(",")
+        .split(',')
         .map(parseFloat);
       return lzwCompress.unpack(buf);
     } catch (err) {
@@ -167,24 +167,20 @@ class Index extends React.Component {
   _getHashStringParams() {
     const query = window.location.hash;
     return query
-      ? (/^[?#]/.test(query) ? query.slice(1) : query)
-          .split("&")
-          .reduce((params, param) => {
-            let [key, value] = param.split("=");
-            params[key] = value
-              ? decodeURIComponent(value.replace(/\+/g, " "))
-              : "";
-            return params;
-          }, {})
+      ? (/^[?#]/.test(query) ? query.slice(1) : query).split('&').reduce((params, param) => {
+          let [key, value] = param.split('=');
+          params[key] = value ? decodeURIComponent(value.replace(/\+/g, ' ')) : '';
+          return params;
+        }, {})
       : {};
   }
 
   _getLastContent() {
-    return localStorage.getItem("lastContent");
+    return localStorage.getItem('lastContent');
   }
 
   _setLastContent(content) {
-    localStorage.setItem("lastContent", content);
+    localStorage.setItem('lastContent', content);
   }
 
   _tryEval(content) {
@@ -198,7 +194,7 @@ class Index extends React.Component {
       // content
       eval(
         `${prelude};
-        ${content}`
+        ${content}`,
       );
       return true;
     } catch (err) {
@@ -287,7 +283,7 @@ class Index extends React.Component {
     const { audioContext, isPlaying, generator, isFlashing } = this.state;
 
     return (
-      <div className={isFlashing ? "flash" : ""}>
+      <div className={isFlashing ? 'flash' : ''}>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
@@ -320,9 +316,9 @@ class Index extends React.Component {
             isPlaying={isPlaying}
           />
         ) : (
-          ""
+          ''
         )}
-        {this.state.error ? <ErrorMessage message={this.state.error} /> : ""}
+        {this.state.error ? <ErrorMessage message={this.state.error} /> : ''}
 
         <style global jsx>{`
           body {
